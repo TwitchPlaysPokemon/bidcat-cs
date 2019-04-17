2023f65365c672ff2282c1d0de880a802cfd371d
red031000 <rubenru09@aol.com>
2019-04-17 16:34:00 +0100

    add support for multiple banks and auctions and more api endpoints

--- BidCat/API/Auction.cs
+++ BidCat/API/Auction.cs
@@ -9,40 +9,179 @@ namespace BidCat.API
 {
 	public class Auction
 	{
-		Dictionary<string, Dictionary<int, int>> lotDict = new Dictionary<string, Dictionary<int, int>>();
+		// Lot ID, User ID, Amount
+		private Dictionary<string, List<Tuple<int, int>>> lotDict = new Dictionary<string, List<Tuple<int, int>>>();
+
+		private List<string> ChangesTracker = new List<string>();
 
 		AbstractBank bank;
-		public Auction(Action<ApiLogMessage> logger)
+		public Auction(Action<ApiLogMessage> logger, AbstractBank bank)
 		{
-			switch (Listener.Config.StorageType)
-			{
-				case StorageType.Memory:
-					bank = new MemoryBank(logger);
-					break;
-				case StorageType.Mongo:
-					bank = new MongoBank(logger);
-					break;
-				case StorageType.Postgres:
-					bank = new PostgresBank(logger);
-					break;
-			}
-
+			this.bank = bank;
+			this.bank.RegisterReservedMoneyChecker(GetReservedMoney);
 		}
 
-		public async Task<int> GetReservedMoney(int userId)
+		private async Task<int> GetReservedMoney(int userId)
 		{
 			return (await GetBidsForUser(userId)).Sum(x => x.Value);
 		}
 
+		public async Task<Dictionary<int, int>> GetReservedMoney(IEnumerable<int> userIds)
+		{
+			Dictionary<int, int> ret = new Dictionary<int, int>();
+			foreach (int userId in userIds)
+				ret.Add(userId, await GetReservedMoney(userId));
+			return ret;
+		}
+
+#pragma warning disable 1998
 		public async Task<Dictionary<string, int>> GetBidsForUser(int userId)
 		{
 			Dictionary<string, int> bids = new Dictionary<string, int>();
-			foreach (KeyValuePair<string, Dictionary<int, int>> pair in lotDict)
+			foreach (KeyValuePair<string, List<Tuple<int, int>>> pair in lotDict)
 			{
-				if (pair.Value.ContainsKey(userId))
-					bids.Add(pair.Key, pair.Value[userId]);
+				if (pair.Value.Any(x => x.Item1 == userId))
+					bids.Add(pair.Key, pair.Value.First(x => x.Item1 == userId).Item2);
 			}
 			return bids;
+		}
+#pragma warning restore 1998
+
+		public async Task<Dictionary<int, Dictionary<string, int>>> GetBidsForUsers(IEnumerable<int> userIds)
+		{
+			Dictionary<int, Dictionary<string, int>> ret = new Dictionary<int, Dictionary<string, int>>();
+			foreach (int userId in userIds)
+				ret.Add(userId, await GetBidsForUser(userId));
+			return ret;
+		}
+
+#pragma warning disable 1998
+		public async Task Clear()
+		{
+			lotDict.Clear();
+		}
+#pragma warning restore 1998
+
+		public async Task PlaceBid(int user, string item, int amount)
+		{
+			await HandleBid(user, item, amount);
+		}
+
+		public async Task PlaceBids(IEnumerable<Tuple<int, string, int>> bids)
+		{
+			foreach ((int user, string item, int amount) in bids)
+				await PlaceBid(user, item, amount);
+		}
+
+		private void UpdateLastChange(string item)
+		{
+			if (ChangesTracker.Contains(item))
+				ChangesTracker.Remove(item);
+			ChangesTracker.Add(item);
+		}
+
+		private async Task HandleBid(int user, string item, int amount, bool replace = false,
+			bool allowVisibleLowering = true)
+		{
+			if (amount < 1)
+			{
+				throw new ApiError("Amount must be a number above 0");
+			}
+
+			int? previousBid = null;
+			if (lotDict.TryGetValue(item, out _))
+			{
+				previousBid = lotDict[item].FirstOrDefault(x => x.Item1 == user)?.Item2;
+			}
+
+			bool alreadyBid = previousBid != null;
+
+			if (!replace && alreadyBid)
+				throw new AlreadyBidError("There is already a bid from that user on that item.");
+			if (replace && !alreadyBid)
+				throw new NoExistingBidError("There is no bid from that user on that item which could be replaced.");
+			if (replace && previousBid == amount)
+				return;
+			int neededMoney = amount;
+			if (replace)
+				neededMoney -= (int)previousBid;
+			int availableMoney = await bank.GetAvailableMoney(user);
+			if (neededMoney > availableMoney)
+				throw new InsufficientMoneyError($"Can't afford to bid {neededMoney}, only {availableMoney} available.");
+			if (replace && amount < previousBid && !allowVisibleLowering)
+			{
+				Dictionary<string, object> winner = await GetWinner();
+				if ((string)winner["item"] != item)
+					throw new VisiblyLoweredError();
+				int headroom = (int) winner["total_bid"] - (int) winner["total_charge"];
+				int decrease = (int)previousBid - amount;
+				if (decrease > headroom)
+					throw new VisiblyLoweredError();
+			}
+
+			UpdateLastChange(item);
+			if (!lotDict.ContainsKey(item))
+				lotDict[item] = new List<Tuple<int, int>>();
+			lotDict[item].Add(new Tuple<int, int>(user, amount));
+		}
+
+#pragma warning disable 1998
+		public async Task<List<Tuple<string, List<Tuple<int, int>>>>> GetAllBidsOrdered()
+		{
+			List<Tuple<string, List<Tuple<int, int>>>> result = new List<Tuple<string, List<Tuple<int, int>>>>();
+			foreach (KeyValuePair<string, List<Tuple<int, int>>> kvp in lotDict)
+			{
+				Tuple<string, List<Tuple<int, int>>> tuple =
+					new Tuple<string, List<Tuple<int, int>>>(kvp.Key, kvp.Value);
+				result.Add(tuple);
+			}
+
+			return result.OrderByDescending(x => x.Item2.Sum(y => y.Item2)).ToList();
+		}
+#pragma warning restore 1998
+
+		public async Task<Dictionary<string, object>> GetWinner(bool discountLatter = false)
+		{
+			List<Tuple<string, List<Tuple<int, int>>>> bids = await GetAllBidsOrdered();
+			if (!bids.Any())
+				return null;
+			(string winningItem, List<Tuple<int, int>> winningBids) = bids[0];
+			int secondBid = 0;
+			if (bids.Count > 1)
+			{
+				List<Tuple<int, int>> secondItemBids = bids[1].Item2;
+				secondBid = secondItemBids.Sum(x => x.Item2);
+			}
+
+			int totalBid = winningBids.Sum(x => x.Item2);
+			int overpaid = Math.Max(0, totalBid - secondBid - 1);
+			int totalCharge = totalBid - overpaid;
+			
+			List<Tuple<int, int>> moneyOwed = (from userAmountTuple in winningBids.OrderByDescending(x => x.Item2) let percentage = userAmountTuple.Item2 / (double) totalBid select new Tuple<int, int>(userAmountTuple.Item1, (int) Math.Ceiling(totalCharge * percentage))).ToList();
+
+			overpaid = moneyOwed.Sum(x => x.Item2) - totalCharge;
+			List<Tuple<int, int>> array = new List<Tuple<int, int>>();
+			array.AddRange(moneyOwed);
+			if (discountLatter) array.Reverse();
+			for (int i = overpaid; i <= 0;)
+			{
+				foreach (Tuple<int, int> user in array)
+				{
+					Tuple<int, int> temp = moneyOwed.First(x => x.Item1 == user.Item1);
+					Tuple<int, int> temp2 = new Tuple<int, int>(temp.Item1, temp.Item2 - 1);
+					moneyOwed[moneyOwed.IndexOf(temp)] = temp2;
+					i--;
+					if (i == 0) break;
+				}
+			}
+
+			return new Dictionary<string, object>
+			{
+				{ "item", winningItem },
+				{ "total_bid", totalBid },
+				{ "total_charge", totalCharge },
+				{ "money_owed", moneyOwed }
+			};
 		}
 	}
 }
--- BidCat/API/BiddingController.cs
+++ BidCat/API/BiddingController.cs
@@ -1,21 +1,75 @@
 ﻿using ApiListener;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using BidCat.Banks;
+using Newtonsoft.Json;
 
 namespace BidCat.API
 {
 	public class BiddingController : ApiProvider
 	{
-		private Auction auctionController;
-
-		public override IEnumerable<ApiCommand> Commands => new List<ApiCommand>()
+		Dictionary<int, AbstractBank> banks = new Dictionary<int, AbstractBank>();
+		Dictionary<int, Auction> auctions = new Dictionary<int, Auction>();
+		public override IEnumerable<ApiCommand> Commands => new List<ApiCommand>
 		{
+			//The Auction ID must be first in the URL when it's needed, same for the Bank ID
+			new ApiCommand("RegisterBank", args => args.Count() == 3 ? CreateBank(args.ToArray()[0], args.ToArray()[1], args.ToArray()[2]) : throw new ApiError("Incorrect number of argument, expected 3"), new List<ApiParameter>{ new ApiParameter("Users Collection Name", "string"), new ApiParameter("Transactions Collection Name", "string"), new ApiParameter("Currency Field Name", "string") }, "Registers a bank system. Returns the ID of the bank."),
+			new ApiCommand("RegisterAuction", args => args.Any() ? RegisterAuction(int.Parse(args.First())) : throw new ApiError("Bank ID was not specified"), new List<ApiParameter> { new ApiParameter("Bank ID") }, "Registers a new auction."),
+			new ApiCommand("GetReservedMoney", args => GetAuction(args.Any() ? int.Parse(args.First()) : throw new ApiError("Auction ID was not specified")).GetReservedMoney(args.Skip(1).Count() == 1 ? JsonConvert.DeserializeObject<IEnumerable<int>>(args.Skip(1).First()) : args.Skip(1).Any() ? args.Skip(1).Select(int.Parse) : throw new ApiError("User ID was not specified")).Wait(), new List<ApiParameter>{ new ApiParameter("Auction ID"), new ApiParameter("User Id") }, "Finds the total reserved money for a given user."),
+			new ApiCommand("Clear", args => GetAuction(args.Any() ? int.Parse(args.First()) : throw new ApiError("Auction ID was not specified")).Clear().Wait(), new List<ApiParameter> { new ApiParameter("Auction ID") }, "Clears all bids."),
+			new ApiCommand("GetAllBidsOrdered", args => GetAuction(args.Any() ? int.Parse(args.First()) : throw new ApiError("Auction ID was not specified")).GetAllBidsOrdered().Wait(), new List<ApiParameter>{ new ApiParameter("Auction ID") }, "Returns all bids as List<Tuple<string, List<Tuple<int, int>>>>, ordered by ranking (first=winner)"),
+			new ApiCommand("GetBidsForUser", args => GetAuction(args.Any() ? int.Parse(args.First()) : throw new ApiError("Auction ID was not specified")).GetBidsForUser(args.Skip(1).Any() ? int.Parse(args.Skip(1).First()) : throw new ApiError("User ID was not specified")).Wait(), new List<ApiParameter> { new ApiParameter("Auction ID"), new ApiParameter("User ID") }, "Gets all bids on every lot for the given user"),
+			new ApiCommand("GetBidsForUsers", args => GetAuction(args.Any() ? int.Parse(args.First()) : throw new ApiError("Auction ID was not specified")).GetBidsForUsers(args.Skip(1).Count() == 1 ? JsonConvert.DeserializeObject<IEnumerable<int>>(args.Skip(1).First()) : args.Skip(1).Any() ? args.Skip(1).Select(int.Parse) : throw new ApiError("User ID was not specified")).Wait(), new List<ApiParameter> { new ApiParameter("Auction ID"), new ApiParameter("User IDs", "IEnumerable<int>") }, "Gets all bids on every lot for the given users"),
+			new ApiCommand("PlaceBid", args => GetAuction(args.Any() ? int.Parse(args.First()) : throw new ApiError("Auction ID was not specified")).PlaceBid(args.Skip(1).Any() ? int.Parse(args.Skip(1).First()) : throw new ApiError("User ID was not specified"), args.Skip(2).Any() ? args.Skip(2).First() : throw new ApiError("Lot ID was not specified"), args.Skip(3).Any() ? int.Parse(args.Skip(3).First()) : throw new ApiError("Amount was not specified")).Wait(), new List<ApiParameter> { new ApiParameter("Auction ID"), new ApiParameter("User ID"), new ApiParameter("Lot ID", "string"), new ApiParameter("Amount") }, "Places a bid on the specified lot"),
+			new ApiCommand("PlaceBids", args => GetAuction(args.Any() ? int.Parse(args.First()) : throw new ApiError("Auction ID was not specified")).PlaceBids(args.Skip(1).Any() ? JsonConvert.DeserializeObject<List<Tuple<int, string, int>>>(args.Skip(1).First()) : throw new ApiError("Bids were not specified")).Wait(), new List<ApiParameter> { new ApiParameter("Auction ID"), new ApiParameter("Bids", "List<Tuple<int, string, int>>")}, "Places bids for multiple users, the first item of each tuple is the User ID, the second is the Lot ID, the third is the Amount"),
+			new ApiCommand("GetWinner", args => GetAuction(args.Any() ? int.Parse(args.First()) : throw new ApiError("Auction ID was not specified")).GetWinner(args.Skip(1).Any() ? bool.Parse(args.Skip(1).First()) : false).Wait(), new List<ApiParameter>{ new ApiParameter("Discount Later bidders", "bool", true) }, "Gets the winner of the given auction")
 		};
 
 		public BiddingController(Action<ApiLogMessage> logger)
 			: base(logger)
+		{ }
+
+		private int CreateBank(string usersCollectionName, string transactionsCollectionName, string fieldName)
 		{
-			auctionController = new Auction(logger);
+			Random random = new Random();
+			int randInt = random.Next();
+			while (banks.ContainsKey(randInt))
+				randInt = random.Next();
+			AbstractBank bank = null;
+			switch (Listener.Config.StorageType)
+			{
+				case StorageType.Memory:
+					bank = new MemoryBank(Log);
+					break;
+				case StorageType.Mongo:
+					bank = new MongoBank(Log, usersCollectionName, transactionsCollectionName, fieldName);
+					break;
+				case StorageType.Postgres:
+					bank = new PostgresBank(Log);
+					break;
+			}
+			banks.Add(randInt, bank);
+			return randInt;
+		}
+
+		private int RegisterAuction(int bankId)
+		{
+			Random random = new Random();
+			int randInt = random.Next();
+			while (auctions.ContainsKey(randInt))
+				randInt = random.Next();
+			if (!banks.ContainsKey(bankId))
+				throw new ApiError("Bank with given ID does not exist");
+			auctions.Add(randInt, new Auction(Log, banks[bankId]));
+			return randInt;
+		}
+
+		private Auction GetAuction(int auctionID)
+		{
+			if (!auctions.ContainsKey(auctionID))
+				throw new ApiError("Auction with given ID does not exist");
+			return auctions[auctionID];
 		}
 	}
 }
